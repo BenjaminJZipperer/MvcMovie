65b31db9046acef975501466400d9d05786716b6
ben@web.de <ben@web.de>
2020-04-19 14:24:42 +0200

    Empty upload methoad

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using MvcMovie.Models;
 
 namespace MvcMovie.Controllers
 {
+    // Hinzufügen einer IActionResult Upload() Methode an das Ende des Controllers 
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -27,11 +28,16 @@ namespace MvcMovie.Controllers
         {
             return View();
         }
-
+        public IActionResult Upload()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
 }
--- Controllers/StartController.cs
+++ Controllers/StartController.cs
@@ -1,3 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcMovie.Models;
+// Deploy https://docs.microsoft.com/en-us/azure-stack/user/azure-stack-dev-start-howto-vm-dotnet?view=azs-2002
+
+
 namespace MvcMovie.Controllers
 {
    public class StartController : Controller
