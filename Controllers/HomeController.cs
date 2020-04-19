04debd2d83d3f5fa4729afe7fb44dba68c83ed35
ben@web.de <ben@web.de>
2020-04-19 14:36:48 +0200

    Change Frontend

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MvcMovie.Models;
-
+using Microsoft.AspNetCore.Http;
 namespace MvcMovie.Controllers
 {
     // Hinzufügen einer IActionResult Upload() Methode an das Ende des Controllers 
